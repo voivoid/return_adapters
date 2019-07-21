a155fa9509915c05092ea615040809979e588634
Sergey Ivanov <voivoid@mail.ru>
2019-07-21 22:14:21 +0300

    throwing adapter stub

---
+++ tests/throwing_adapter.cpp
@@ -0,0 +1,15 @@
+#include "catch2/catch.hpp"
+
+#include "return_adapters/throwing_adapter.h"
+
+namespace
+{
+  bool return_false_if_greater_than_10( int v )
+  {
+    return v > 10;
+  }
+}
+
+TEST_CASE("Check a throwing adapted function", "throwing_adapter")
+{
+}
