a2f2f84e272b5e11896c2acf221d26a5a931edc8
Sergey Ivanov <voivoid@mail.ru>
2019-07-22 10:47:12 +0300

    add simple throwing adapter

--- inc/return_adapters/throwing_adapter.h
+++ inc/return_adapters/throwing_adapter.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <utility>
+
+namespace details
+{
+template <auto* adaptee_func, typename FuncType, typename RetValHandler>
+struct throwing_adapter;
+
+template <auto* adaptee_func, typename Ret, typename... Args, typename RetValHandler>
+struct throwing_adapter<adaptee_func, Ret ( * )( Args... ), RetValHandler>
+{
+  static auto throwing_func( Args... args )
+  {
+    return RetValHandler()( adaptee_func( std::forward<Args>( args )... ) );
+  }
+};
+
+}  // namespace details
+
+template <auto* func, typename RetValHandler>
+constexpr auto* adapt_to_throwing_func()
+{
+    return &details::throwing_adapter<func, decltype( func ), RetValHandler>::throwing_func;
+}
--- tests/throwing_adapter.cpp
+++ tests/throwing_adapter.cpp
@@ -2,14 +2,39 @@
 
 #include "return_adapters/throwing_adapter.h"
 
+#include <stdexcept>
+
 namespace
 {
-bool return_false_if_greater_than_10( int v )
+enum class result
 {
-  return v > 10;
+  success,
+  failure
+};
+
+int open_file( const result expected_result )
+{
+  return expected_result == result::success ? 0 : -1;
 }
+
+struct throw_if_nonzero_result
+{
+    void operator()( const int result ) const
+    {
+        if( result != 0 )
+        {
+            throw std::runtime_error( "operation failed" );
+        }
+    }
+};
+
 }  // namespace
 
 TEST_CASE( "Check a throwing adapted function", "throwing_adapter" )
 {
+    constexpr auto* throwing_func = adapt_to_throwing_func<&open_file, throw_if_nonzero_result>();
+    REQUIRE( throwing_func );
+
+    CHECK_NOTHROW( throwing_func( result::success ));
+    CHECK_THROWS_AS( throwing_func( result::failure ), std::runtime_error );
 }
