d605a0f6824fb2498255e88ed35ab2007022e824
Sergey Ivanov <voivoid@mail.ru>
2019-07-29 20:44:59 +0300

    add typestring thirdparty; move thirdparties to a separate dir

---
+++ cmake/thirdparties/typestring.cmake
@@ -0,0 +1,25 @@
+include(ExternalProject)
+
+find_package(Git REQUIRED)
+
+set(TypeStringVer master)
+
+ExternalProject_Add(
+  get_typestring
+  PREFIX thirdparties
+  URL https://github.com/irrequietus/typestring/archive/${TypeStringVer}.zip
+  URL_HASH SHA256=2b21bbd470809a7701849964a00ad0bf875eba58c67bf546c2ec26fc8d17ef4f
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  UPDATE_COMMAND ""
+  )
+
+add_library(typestring INTERFACE)
+add_library(TypeString::TypeString ALIAS typestring)
+
+ExternalProject_Get_Property(get_typestring SOURCE_DIR)
+set_target_properties(typestring PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${SOURCE_DIR})
+set_target_properties(typestring PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${SOURCE_DIR})
+
+add_dependencies(typestring get_typestring)
