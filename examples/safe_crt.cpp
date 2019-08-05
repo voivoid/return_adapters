654be54435f0f63ad12e36f40ab3f9756c9684ec
Sergey Ivanov <voivoid@mail.ru>
2019-08-05 13:24:23 +0300

    added retval to_optional handler

--- examples/safe_crt.cpp
+++ examples/safe_crt.cpp
@@ -10,13 +10,17 @@ namespace safe_crt
 using namespace return_adapters;
 using namespace return_adapters::throwing;
 
-template <typename retval_checker>
-using safe_crt_throwing_handler = generic_adapter_handler<retval_checker, errno_str_exception_formatter, std::runtime_error>;
+template <typename retval_predicate>
+using safe_crt_throwing_handler = generic_adapter_handler<retval_predicate, errno_str_exception_formatter, std::runtime_error>;
 
-constexpr auto* fclose = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fclose, safe_crt_throwing_handler<check_retval_is_zero> );
-//constexpr auto* feof   = retval::adapt<&::feof, check_retval_is_zero>();
-constexpr auto* fflush = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fflush, safe_crt_throwing_handler<check_retval_is_zero> );
-constexpr auto* fopen  = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fopen, safe_crt_throwing_handler<check_ret_ptr_is_not_null> );
-constexpr auto* fseek  = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fseek, safe_crt_throwing_handler<check_retval_is_zero> );
+constexpr auto* fclose  = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fclose, safe_crt_throwing_handler<check_retval_is_zero> );
+constexpr auto* feof    = retval::adapt<::feof, check_retval_is_zero>();
+constexpr auto* ferror  = retval::adapt<::ferror, retval::to_optional<check_retval_is_not_zero, retval::adapter_id>>();
+constexpr auto* fflush  = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fflush, safe_crt_throwing_handler<check_retval_is_zero> );
+//constexpr auto* fgetc   = TODO
+constexpr auto* fgetpos = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fgetpos, safe_crt_throwing_handler<check_retval_is_zero> );
+constexpr auto* fgets   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fgets, safe_crt_throwing_handler<check_ret_ptr_is_not_null> );
+constexpr auto* fopen   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fopen, safe_crt_throwing_handler<check_ret_ptr_is_not_null> );
+constexpr auto* fseek   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fseek, safe_crt_throwing_handler<check_retval_is_zero> );
 
 }  // namespace safe_crt
--- inc/return_adapters/retval_adapter.h
+++ inc/return_adapters/retval_adapter.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <utility>
+#include <optional>
+#include <type_traits>
 
 namespace return_adapters
 {
@@ -9,7 +11,6 @@ namespace retval
 
 namespace details
 {
-
 template <auto* adaptee_func, typename FuncType, typename RetValAdapter>
 struct retval_adapter;
 
@@ -22,7 +23,42 @@ struct retval_adapter<adaptee_func, Ret ( * )( Args... ), RetValAdapter>
   }
 };
 
+template <auto* adaptee_func, typename Ret, typename... Args, typename RetValAdapter>
+struct retval_adapter<adaptee_func, Ret ( * )( Args... ) throw (), RetValAdapter>
+{
+  static auto retval_adapted_func( Args... args )
+  {
+    return RetValAdapter()( adaptee_func( std::forward<Args>( args )... ) );
+  }
+};
+
 }  // namespace details
+
+struct adapter_id
+{
+    template <typename T>
+    const T& operator()( const T& t ) const
+    {
+        return t;
+    }
+};
+
+template <typename Predicate, typename Adapter>
+struct to_optional
+{
+    template<typename T>
+    auto operator()( const T& retval ) const
+    {
+        using AdaptedType = std::decay_t<decltype(Adapter{}( retval ))>;
+        using OPT = std::optional<AdaptedType>;
+        if( Predicate{}( retval ) )
+        {
+            return OPT{ Adapter{}( retval ) };
+        }
+
+        return OPT{};
+    }
+};
 
 template <auto* func, typename RetValAdapter>
 constexpr auto* adapt()
