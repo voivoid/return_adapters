b385a35deec02e8dd99285f5c9d9fa752a1ec3ed
Sergey Ivanov <voivoid@mail.ru>
2019-08-07 20:57:00 +0300

    add safe_com examples

--- examples/safe_com.cpp
+++ examples/safe_com.cpp
@@ -1,8 +1,37 @@
-#include "return_adapters/retval_adapter.h"
+#include "return_adapters/predicates.h"
+#include "return_adapters/throwing_adapter.h"
+#include "return_adapters/out_retval_adapter.h"
+
+#include <string>
 
 #include <comdef.h>
 
 namespace safe_com
 {
 
-}
+using namespace return_adapters;
+
+struct hresult_str_exception_formatter
+{
+  std::string operator()( const char* const adaptee_func_name, const HRESULT result ) const
+  {
+    return throwing::details::format_error_message( adaptee_func_name, "hresult", std::to_string( result ) );
+  }
+};
+
+struct is_hresult_succeeded_predicate
+{
+    bool operator()( const HRESULT result ) const
+    {
+      return SUCCEEDED( result );
+    }
+};
+
+using safe_com_throwing_handler = throwing::generic_adapter_handler<is_hresult_succeeded_predicate, hresult_str_exception_formatter, std::runtime_error>;
+
+constexpr auto CoInitialize     = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::CoInitialize, safe_com_throwing_handler );
+constexpr auto CoCreateGuid     = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::CoCreateGuid, safe_com_throwing_handler );
+constexpr auto CoCreateInstance = out_retval::adapt<&::CoCreateInstance, out_retval::out_retval_optional_adapter<is_hresult_succeeded_predicate>>();
+constexpr auto IIDFromString    = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::IIDFromString, safe_com_throwing_handler );
+constexpr auto StringFromIID    = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::StringFromIID, safe_com_throwing_handler );
+}  // namespace safe_com
