2e6afe4bc21f553b0de38c1b8808256a628cf92c
Sergey Ivanov <voivoid@mail.ru>
2020-05-03 18:41:33 +0300

    add varargs for throwing adapters

--- examples/safe_crt.cpp
+++ examples/safe_crt.cpp
@@ -31,18 +31,19 @@ using safe_crt_throwing_handler = throwing::generic_adapter_handler<retval_predi
 
 constexpr auto fclose  = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fclose, safe_crt_throwing_handler<check_retval_is_zero> );
 constexpr auto feof    = map_retval<::feof, check_retval_is_zero>;
-constexpr auto ferror  = map_retval<::ferror, retval::to_optional<check_retval_is_not_zero>>;
+constexpr auto ferror  = map_retval<::ferror, check_retval_is_zero>;
 constexpr auto fflush  = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fflush, safe_crt_throwing_handler<check_retval_is_zero> );
 constexpr auto fgetc   = map_retval<::fgetc, retval::to_optional<check_retval_is_not_<EOF>>>;
 constexpr auto fgetpos = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fgetpos, safe_crt_throwing_handler<check_retval_is_zero> );
 constexpr auto fgets   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fgets, safe_crt_throwing_handler<check_ret_ptr_is_not_null> );
-constexpr auto fopen   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fopen, safe_crt_throwing_handler<check_ret_ptr_is_not_null> );
-//             fprintf
+constexpr auto fopen   = raii::adapt<RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fopen, safe_crt_throwing_handler<check_ret_ptr_is_not_null> ),
+                                   raii::to_unique_ptr_with_deleter_func_<safe_crt::fclose>>;
+constexpr auto fprintf = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fprintf, safe_crt_throwing_handler<check_retval_is_less_zero> );
 constexpr auto fputc   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fputc, safe_crt_throwing_handler<check_retval_is_not_<EOF>> );
 constexpr auto fputs   = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fputs, safe_crt_throwing_handler<check_retval_is_not_<EOF>> );
 constexpr auto fread   = RETURN_ADAPTERS_ADAPT_TO_THROWING_WITH_INDICES( ::fread, fread_handler, 2, 3 );
 constexpr auto freopen = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::freopen, safe_crt_throwing_handler<check_ret_ptr_is_not_null> );
-//             fscanf
+constexpr auto fscanf = RETURN_ADAPTERS_ADAPT_TO_THROWING(::fscanf, safe_crt_throwing_handler<check_retval_is_not_<EOF>>);
 constexpr auto fseek = RETURN_ADAPTERS_ADAPT_TO_THROWING( ::fseek, safe_crt_throwing_handler<check_retval_is_zero> );
 
 }  // namespace safe_crt
@@ -52,8 +53,8 @@ namespace
 
 void example()
 {
-  FILE* file = safe_crt::fopen( "file.txt", "r" );
-  safe_crt::fclose( file );
+  auto file = safe_crt::fopen( "file.txt", "w" ); // returns std::unique_ptr<FILE> with ::fclose deleter
+  safe_crt::fprintf( file.get(), "HELLO, %s\n", "WORLD" );
 }
 
 }  // namespace
--- inc/return_adapters/predicates.h
+++ inc/return_adapters/predicates.h
@@ -5,38 +5,49 @@
 namespace return_adapters
 {
 
-template <auto expected>
-struct check_retval_is_
+template <typename Check>
+struct negate
 {
   template <typename RetVal>
-  bool operator()( const RetVal& retval ) const
+  bool operator()( RetVal retval ) const
   {
-    return retval == expected;
+    return !Check{}( retval );
+  }
+};
+
+template <template <typename> class Predicate, auto expected>
+struct check_retval
+{
+  template <typename RetVal>
+  bool operator()( const RetVal retval ) const
+  {
+    return Predicate<decltype( retval )>{}( retval, expected );
   }
 };
 
 template <auto expected>
-struct check_retval_is_not_
-{
-  template <typename RetVal>
-  bool operator()( const RetVal& retval ) const
-  {
-    return !check_retval_is_<expected>{}( retval );
-  }
-};
+using check_retval_is_ = check_retval<std::equal_to, expected>;
+
+template <auto expected>
+using check_retval_is_not_ = negate<check_retval_is_<expected>>;
+
+template <auto expected>
+using check_retval_is_less_ = check_retval<std::less, expected>;
 
 struct id
 {
   template <typename T>
-  auto&& operator()( T&& t ) const
+  decltype( auto ) operator()( T&& t ) const
   {
     return std::forward<T>( t );
   }
 };
 
 using check_retval_is_zero      = check_retval_is_<0>;
+using check_retval_is_nullptr   = check_retval_is_<nullptr>;
 using check_retval_is_true      = check_retval_is_<true>;
 using check_retval_is_not_zero  = check_retval_is_not_<0>;
 using check_ret_ptr_is_not_null = check_retval_is_not_<nullptr>;
+using check_retval_is_less_zero = check_retval_is_less_<0>;
 
 }  // namespace return_adapters
